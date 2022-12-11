7dd141ee75674694430bcffd84d57a71f37b5f71
IgnacioBMS <111017465+IgnacioBMS@users.noreply.github.com>
2022-12-11 19:53:27 -0300

    cbj.
    vn.

--- list.js
+++ list.js
@@ -1,10 +1,10 @@
 document.write('hola\n')
-var lista = [];
-var archivo = new XMLHttpRequest();
-var file = 'lista.txt';
-archivo.open('GET',file,false);
-archivo.send(null);
-var nombres = archivo.responseText;
-lista = nombres.split(';')
-document.write(lista)
+//var lista = [];
+//var archivo = new XMLHttpRequest();
+//var file = 'lista.txt';
+//archivo.open('GET',file,false);
+//archivo.send(null);
+//var nombres = archivo.responseText;
+//lista = nombres.split(';')
+//document.write(lista)
 document.write('chao\n')
