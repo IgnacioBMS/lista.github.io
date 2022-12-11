e86e76536783a5899515748cf47a1fb2e162a194
IgnacioBMS <111017465+IgnacioBMS@users.noreply.github.com>
2022-12-11 20:10:21 -0300

    rsi
    hjs

--- list.js
+++ list.js
@@ -3,5 +3,6 @@ var lista = [];
 var archivo = new XMLHttpRequest();
 var file = 'lista.csv';
 archivo.open('GET',file,false);
+archivo.send(null);
 var nombres = archivo.responseText;
 document.write(nombres)
