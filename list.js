8e5050b114978f5a2e00cd8c65fb29da2cf7fc45
IgnacioBMS <111017465+IgnacioBMS@users.noreply.github.com>
2022-12-11 19:37:05 -0300

    dfhmk
    dgh

--- list.js
+++ list.js
@@ -3,5 +3,5 @@ var archivo = new XMLHttpRequest();
 var file = 'lista.txt';
 archivo.open('GET',file,false);
 archivo.send(null);
-var lista = archivo.responseText;
-document.write(lista)
+var nombres = archivo.responseText;
+nombres.strip('\n')
